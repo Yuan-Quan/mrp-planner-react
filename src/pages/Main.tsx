3107637105e863c37f2c5be6081c5e8fa32dab6f
Quan <metroyuan@gamil.com>
2023-10-23 15:42:53 +0800

    minor changes

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,13 +4,16 @@ import { Link } from "react-router-dom"
 export const Main = () => {
     return (
         <Box>
-            <Typography variant="h1">Main page place holder</Typography>
+            <Typography variant="h4">Material Resource Planning (MRP) Calculator</Typography>
             <List>
                 <ListItem>
                     <Link to="/config/target">开始设定参数</Link>
                 </ListItem>
                 <ListItem>
                     <Link to="/result">计算结果</Link>
+                </ListItem>
+                <ListItem>
+                    <Link to="/clear">清除设定</Link>
                 </ListItem>
             </List>
         </Box>
